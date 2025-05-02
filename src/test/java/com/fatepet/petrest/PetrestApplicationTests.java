cc4aeb7832ecfeeafd52b3c48bb3874cfd05f11e
sangjun <sangjoon12345@gmail.com>
2025-05-02 18:03:42 +0900

    Initial commit

---
+++ src/test/java/com/fatepet/petrest/PetrestApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.fatepet.petrest;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class PetrestApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
