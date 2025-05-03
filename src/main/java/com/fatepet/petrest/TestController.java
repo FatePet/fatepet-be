d30edc1797e4b2a0289601d0411bbf43305093b7
sangjun <sangjoon12345@gmail.com>
2025-05-03 10:57:13 +0900

    feat: application.yml(로컬, 배포 환경 구성)

--- src/main/java/com/fatepet/petrest/TestController.java
+++ src/main/java/com/fatepet/petrest/TestController.java
@@ -1,8 +1,13 @@
 package com.fatepet.petrest;
 
 import jakarta.ws.rs.GET;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @RestController
 public class TestController {
@@ -10,5 +15,17 @@ public class TestController {
     @GetMapping("/test")
     public String test(){
         return "hello";
+    }
+
+    @Autowired
+    private DataSource dataSource;
+
+    @GetMapping("/health/db")
+    public String checkDbConnection() {
+        try (Connection conn = dataSource.getConnection()) {
+            return "DB 연결 성공!";
+        } catch (SQLException e) {
+            return "DB 연결 실패: " + e.getMessage();
+        }
     }
 }
