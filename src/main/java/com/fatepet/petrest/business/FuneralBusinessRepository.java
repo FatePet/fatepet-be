168afaa76e36bea8d1a5cef331028d93773a1ea5
ehgk4245 <ehgkrk14@naver.com>
2025-05-15 20:19:59 +0900

    feat: 업체명 중복 확인 기능 추가

--- src/main/java/com/fatepet/petrest/business/FuneralBusinessRepository.java
+++ src/main/java/com/fatepet/petrest/business/FuneralBusinessRepository.java
@@ -3,4 +3,6 @@ package com.fatepet.petrest.business;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FuneralBusinessRepository extends JpaRepository<FuneralBusiness, Long> {
+
+    Boolean existsByName(String name);
 }
