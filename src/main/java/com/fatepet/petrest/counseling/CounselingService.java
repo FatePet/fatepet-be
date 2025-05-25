ba53183bfadeb13f58aefd0ccb365188875b010e
sj0320 <112684207+sj0320@users.noreply.github.com>
2025-05-25 13:04:19 +0900

    Update CounselingService.java

--- src/main/java/com/fatepet/petrest/counseling/CounselingService.java
+++ src/main/java/com/fatepet/petrest/counseling/CounselingService.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class CounselingService {
 
@@ -27,7 +26,7 @@ public class CounselingService {
         FuneralBusiness business = funeralBusinessRepository.findById(businessId)
                 .orElseThrow(() -> new FuneralBusinessException(ResponseCode.NOT_FOUND));
 
-        String businessPhoneNumber = business.getPhoneNumber().replaceAll("-", "");
+        String businessPhoneNumber = business.getPhoneNumber();
         String businessEmail = business.getEmail();
         String contactTypeName = ContactType.getDisplayNameByKey(contactType);
 
